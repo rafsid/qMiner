bbe426a14f3a49f19ab2801763a583bf49cb312e
Your Name <mdrafat.siddiqui@gmail.com>
2024-08-02 20:20:24 +0530

    Added user-agent header and updated request with headers in ws.py.

--- ws.py
+++ ws.py
@@ -3,19 +3,19 @@ import requests
 
 url = "https://www.walmart.com/ip/Lenovo-IdeaCentre-3i-27-inch-FHD-IPS-Touch-All-in-One-Desktop-Intel-Core-i5-12450H-8GB-RAM-512GB-SSD-Black/2183074919?adsRedirect=true"
 
-response = requests.get(url)
-
 HEADERS = {
     
     "Accept": "*/*", 
     "Accept-Encoding": "gzip, deflate, br, zstd",
-    "Accept-Language": "en-GB,en-US;q=0.9,en;q=0.8"
+    "Accept-Language": "en-GB,en-US;q=0.9,en;q=0.8",
+    "user-agent" : "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/127.0.0.0 Safari/537.36"
 }
 
+response = requests.get(url, headers=HEADERS)
 
 soup = BeautifulSoup(response.text, "html.parser")
 
 script_tag = soup.find("script", id="__NEXT_DATA__")
-html = script_tag.string
+html = script_tag
 
 print(html)
